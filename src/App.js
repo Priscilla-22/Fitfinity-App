50ea840ad83f5fdd0b35e498c64bb67a8d08ff69
Priscilla-22 <priscillamw123@gmail.com>
2024-02-14 12:28:14 +0300

    feat: Integrate ExerciseCarousel component into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import ImageSlider from './Components/ImageSlider/ImageSlider.jsx'; 
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className='App'>
+      <h1>My Exercise Image Carousel</h1>
+      <ImageSlider />
     </div>
   );
 }

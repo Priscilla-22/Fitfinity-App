f71fa944347aed917b9a203a4069d6d176a787cf
Priscilla-22 <priscillamw123@gmail.com>
2024-02-14 13:48:05 +0300

    feat: implement the workoutPlanList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+// import React from 'react';
+// import ImageSlider from './Components/ImageSlider/ImageSlider.jsx'
+// import WorkoutPlans from './Components/WorkoutPlans/WorkoutPlans.jsx';
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <ImageSlider />
+//       <h1 className='text-4xl font-bold text-center my-8'>My Exercise Plan</h1>
+//       <WorkoutPlans />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import ImageSlider from './Components/ImageSlider/ImageSlider.jsx'; 
+import ImageSlider from './Components/ImageSlider/ImageSlider.jsx';
+import WorkoutPlanList from './Components/WorkoutPlanList/WorkoutPlanList.jsx';
 
 function App() {
   return (
     <div className='App'>
-      <h1>My Exercise Image Carousel</h1>
       <ImageSlider />
+     <h1 className='text-4xl font-bold text-center my-8'>My Exercise Plan</h1>
+      <WorkoutPlanList />
     </div>
   );
 }

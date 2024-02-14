283861d9362853a47e72932d792784d81b01e2f6
Priscilla-22 <priscillamw123@gmail.com>
2024-02-14 17:05:06 +0300

    chore:remove h1

--- src/Components/WorkoutPlanList/WorkoutPlanList.jsx
+++ src/Components/WorkoutPlanList/WorkoutPlanList.jsx
@@ -20,7 +20,7 @@ const WorkoutPlanList = () => {
 
   return (
     <div className='max-w-screen-xl mx-auto'>
-      <h1 className='text-4xl font-bold mb-8'>Workout Plans</h1>
+      {/* <h1 className='text-4xl font-bold mb-8'>Workout Plans</h1> */}
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
         {workoutPlans &&
           workoutPlans.map((workoutPlan) => (

6caf0cd285936a9e9b3be1af22342e26b0064a51
Priscilla-22 <priscillamw123@gmail.com>
2024-02-15 10:58:48 +0300

    style: center the kickstart text

--- src/Components/WorkoutPlans/WorkoutPlans.jsx
+++ src/Components/WorkoutPlans/WorkoutPlans.jsx
@@ -8,61 +8,72 @@ const WorkoutPlan = ({ workoutPlan }) => {
   };
 
   return (
-    <div className='bg-white shadow-[0_35px_60px_-15px_rgba(1.0,1.0,1.0,1.3)] rounded-lg p-4 mb-4 relative'>
-      <img
-        src={workoutPlan.gifUrl}
-        alt={workoutPlan.name}
-        className='mb-2'
-        style={{ maxWidth: '300px' }}
-      />
-      <div className='mb-2'>
-        <h2 className='text-2xl font-bold'>{workoutPlan.name.toUpperCase()}</h2>
-        <p className='text-gray-600'>
-          Difficulty:{' '}
-          <span className='font-bold'>{workoutPlan.difficulty}</span>
-        </p>
-        <div
-          className='text-center mt-2'
-          onClick={toggleExpanded}
-          style={{ cursor: 'pointer', color: '#f04c0c', fontSize: '1.1rem' }}
-        >
-          Learn More ➔
-        </div>
+    <div className='flex bg-white shadow-[0_35px_60px_-15px_rgba(0.5,0.5,0.5,0.5)] rounded-lg p-4 mb-4 relative'>
+      <div className='w-1/3 mr-4'>
+        <img
+          src={workoutPlan.displayImage}
+          alt={workoutPlan.name}
+          className='mb-2'
+          style={{ maxWidth: '300px' }}
+        />
       </div>
-      {expanded && (
-        <div>
-          {workoutPlan.type && (
-            <p className='text-gray-600'>
-              Type: <span className='font-bold'>{workoutPlan.type}</span>
-            </p>
-          )}
-          {workoutPlan.muscle && (
-            <p className='text-gray-600'>
-              Muscle: <span className='font-bold'>{workoutPlan.muscle}</span>
-            </p>
-          )}
-          {workoutPlan.bodyPart && (
-            <p className='text-gray-600'>
-              Body Part:{' '}
-              <span className='font-bold'>{workoutPlan.bodyPart}</span>
-            </p>
-          )}
-          {workoutPlan.equipment && (
-            <p className='text-gray-600'>
-              Equipment:{' '}
-              <span className='font-bold'>{workoutPlan.equipment}</span>
-            </p>
-          )}
-          <h3 className='text-lg font-semibold mt-2'>Instructions:</h3>
-          <ul className='list-disc pl-6'>
-            {workoutPlan.instructions.map((instruction, index) => (
-              <li key={index} className='text-gray-700'>
-                {instruction}
-              </li>
-            ))}
-          </ul>
+      <div className='w-2/3 flex flex-col justify-between'>
+        <div className='mb-auto'>
+          <h2 className='text-2xl font-bold'>
+            {workoutPlan.name.toUpperCase()}
+          </h2>
+          <p className='text-gray-600'>
+            Difficulty:{' '}
+            <span className='font-bold'>{workoutPlan.difficulty}</span>
+          </p>
+          <p className='text-gray-600'>
+            Body Part: <span className='font-bold'>{workoutPlan.bodyPart}</span>
+          </p>
+          <p className='text-gray-600'>
+            Equipment:{' '}
+            <span className='font-bold'>{workoutPlan.equipment}</span>
+          </p>
         </div>
-      )}
+        <div className='text-center my-2'>
+          {expanded ? (
+            <div
+              onClick={toggleExpanded}
+              style={{ cursor: 'pointer', color: '#f04c0c', fontSize: '1.1rem' }}
+            >
+              Close
+            </div>
+          ) : (
+            <div
+              onClick={toggleExpanded}
+              style={{ cursor: 'pointer', color: '#f04c0c', fontSize: '1.1rem' }}
+            >
+              Kick start the exercise ➔
+            </div>
+          )}
+        </div>
+        {expanded && (
+          <div>
+            {workoutPlan.type && (
+              <p className='text-gray-600'>
+                Type: <span className='font-bold'>{workoutPlan.type}</span>
+              </p>
+            )}
+            {workoutPlan.muscle && (
+              <p className='text-gray-600'>
+                Muscle: <span className='font-bold'>{workoutPlan.muscle}</span>
+              </p>
+            )}
+            <h3 className='text-lg font-semibold mt-2'>Instructions:</h3>
+            <ul className='list-disc pl-6'>
+              {workoutPlan.instructions.map((instruction, index) => (
+                <li key={index} className='text-gray-700'>
+                  {instruction}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

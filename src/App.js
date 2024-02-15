ee953a6c0b4b58bfe5659ae4ec856295d172565d
Priscilla-22 <priscillamw123@gmail.com>
2024-02-15 12:11:38 +0300

    feat: implement the work out plan details navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ImageSlider from './Components/ImageSlider/ImageSlider.jsx';
 import WorkoutPlanList from './Components/WorkoutPlanList/WorkoutPlanList.jsx';
+import WorkoutPlanDetails from './Components/WorkoutPlanDetails/WorkoutPlanDetails.jsx';
 
 function App() {
   return (
-    <div className='App'>
-      <ImageSlider />
-     <h1 className='text-4xl font-bold text-center my-12'>My Exercise Plan</h1>
-      <WorkoutPlanList />
-    </div>
+    <Router>
+      <div className='App'>
+        <ImageSlider />
+        <h1 className='text-4xl font-bold text-center my-12'>
+          My Exercise Plan
+        </h1>
+        <Routes>
+          <Route path='/' element={<WorkoutPlanList />} />
+          <Route path='/workout/:id' element={<WorkoutPlanDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

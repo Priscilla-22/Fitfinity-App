57b90abbc800d212b415779823b21e0a850f12a1
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-02-15 22:52:22 +0300

    feat: adding handleClick

--- src/components/FitType.jsx
+++ src/components/FitType.jsx
@@ -4,4 +4,12 @@
 function FitType() {
     const [selectedType, setSelectedType] = useState("");
     const [displayArticle, setDisplayArticle] = useState(null); 
-    const [selectedResource, setSelectedResource] = useState("");}
+    const [selectedResource, setSelectedResource] = useState("");
+
+    function handleClick() {
+        const desiredURL = "https://www.healthline.com/nutrition";
+        window.location.href = desiredURL;
+      }
+
+}
+export default FitType;

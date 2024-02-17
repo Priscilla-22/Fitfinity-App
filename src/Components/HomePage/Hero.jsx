a447be20a8201fc60a1561a9ec144ed0e91e50b3
Priscilla-22 <priscillamw123@gmail.com>
2024-02-17 16:15:43 +0300

    feat: add popup functionality  for the login and sign up form

--- src/Components/HomePage/Hero.jsx
+++ src/Components/HomePage/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import SignUpForm from '../SignupLoginFeatures/SignUpForm';
+import LogInForm from '../SignupLoginFeatures/LoginForm';
 
 const Hero = () => {
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+  const [showLogInForm, setShowLogInForm] = useState(false);
+
+  const toggleSignUpForm = () => {
+    setShowSignUpForm(!showSignUpForm);
+    setShowLogInForm(false);
+  };
+
+  const toggleLogInForm = () => {
+    setShowLogInForm(!showLogInForm);
+    setShowSignUpForm(false);
+  };
+
   return (
     <div
       className='bg-cover bg-center mt-0'
@@ -15,7 +30,7 @@ const Hero = () => {
         <div className='flex justify-between w-full h-14 max-w-screen-lg mx-1 mb-12 px-6 py-3'>
           <Header />
           <div>
-            <ul className='flex space-x-4  font-semibold text-white px-4'>
+            <ul className='flex space-x-4 font-semibold text-white px-4'>
               <li>
                 <Link to='/'>Home</Link>
               </li>
@@ -34,11 +49,41 @@ const Hero = () => {
             </ul>
           </div>
         </div>
-        <div className='text-center '>
+        <div className='text-center'>
           <h2 className='text-4xl font-bold text-white'>
             Get Fit and Stay Healthy
           </h2>
         </div>
+        <div className='flex justify-center items-center mt-5'>
+          <button
+            onClick={toggleSignUpForm}
+            className='bg-orange-600 hover:bg-orange-400 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline mr-4'
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={toggleLogInForm}
+            className='bg-orange-600 hover:bg-orange-400 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline'
+          >
+            Log In
+          </button>
+        </div>
+        {showSignUpForm && (
+          <div className='flex justify-center items-center mt-5'>
+            <div className='mr-4'>
+              <h3 className='text-lg font-bold text-white mb-2'>Sign Up</h3>
+              <SignUpForm />
+            </div>
+          </div>
+        )}
+        {showLogInForm && (
+          <div className='flex justify-center items-center mt-5'>
+            <div>
+              <h3 className='text-lg font-bold text-white mb-2'>Log In</h3>
+              <LogInForm />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );

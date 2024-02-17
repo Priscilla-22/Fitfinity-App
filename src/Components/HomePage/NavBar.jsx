085c62b14d1fe836a5647ec6ee730d363d4bfde2
Priscilla-22 <priscillamw123@gmail.com>
2024-02-17 12:07:20 +0300

    refactor:restore the links in the navbar

--- src/Components/HomePage/NavBar.jsx
+++ src/Components/HomePage/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import {Link, useLocation } from 'react-router-dom';
 import ImageSlider from '../ImageSlider/ImageSlider';
 
 const Navbar = () => {
@@ -9,7 +9,25 @@ const Navbar = () => {
   return (
     <div className={`navbar ${isHomePage ? 'home-navbar' : ''}`}>
       {!isHomePage && <ImageSlider />}
-    
+     <div>
+            <ul className='flex space-x-4 '>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/workout-plans'>Workout Plans</Link>
+              </li>
+              <li>
+                <Link to='/fittype'>Fit Type</Link>
+              </li>
+              <li>
+                <Link to='/progress-tracker'>Progress Tracker</Link>
+              </li>
+              <li>
+                <Link to='/exercise-tracker'>Exercise Tracker</Link>
+              </li>
+            </ul>
+          </div>
     </div>
   );
 };

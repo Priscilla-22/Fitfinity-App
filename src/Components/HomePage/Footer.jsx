fd610cf19311feaa0ffda21f85fd76ae02319e9c
Priscilla-22 <priscillamw123@gmail.com>
2024-02-17 12:56:13 +0300

    refactor: background color to black

--- src/Components/HomePage/Footer.jsx
+++ src/Components/HomePage/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-800 text-white py-8'>
+    <footer className='bg-black bg-opacity-100  text-white py-8'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
           <div>

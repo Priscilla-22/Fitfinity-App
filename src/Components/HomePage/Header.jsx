8c0f79f534dbe7b547cc6ed34b11beca2e1ec989
Priscilla-22 <priscillamw123@gmail.com>
2024-02-17 12:44:04 +0300

    style: span the app text

--- src/Components/HomePage/Header.jsx
+++ src/Components/HomePage/Header.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-7xl mx-2 py-4 px-2 sm:px-6 lg:px-2'>
         <h1 className='text-3xl font-bold text-gray-900'>
           <Link to='/'>
-            FITFINITY <span style={{color:'#f04c0c'}}>APP</span>
+            FITFINITY
+            {/* <span style={{ color: '#f04c0c' }}>APP</span> */}
           </Link>
         </h1>
+        <span className='text-orange-500 font-bold text-3xl w-30 '>APP</span>
       </div>
     </header>
   );
